58bf63c13b272312d92864484355b02c97d63442
Suraj Gharde <154982771+surajgharde@users.noreply.github.com>
2026-02-03 12:19:33 +0530

    Remove duplicate print statement

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, Jenkins Pipeline!");
+        
+        System.out.println("Hello, Jenkins Pipeline!");
 
     }
 
